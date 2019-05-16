c9692b06d978c3ad6eb07880f0a97ecdc28bb9d9
PetejStone <petejstoney@gmail.com>
2019-05-16 13:13:48 -0700

    initial commit and hooked up initial state

---
+++ friends/src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+    characters: [],
+    fetching: false,
+    error: ''
+    // Array characters, Boolean fetching, null error.
+  };
+
+  export const rootReducer =(state=initialState, action) => {
+      return state;
+  }
